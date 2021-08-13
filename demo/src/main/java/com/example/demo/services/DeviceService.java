d3ccf62903eb6fe99a9dbaa0eefa0c45a3c6bab4
Vscrl0 <adbeal@ucdavis.edu>
2021-08-13 07:13:52 -0700

    device constraints added

--- demo/src/main/java/com/example/demo/services/DeviceService.java
+++ demo/src/main/java/com/example/demo/services/DeviceService.java
@@ -7,30 +7,39 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.beans.Device;
-import com.example.demo.beans.User;
+import com.example.demo.beans.UserPlan;
 import com.example.demo.data.DeviceRepository;
 
 @Service
 public class DeviceService {
 	@Autowired
 	private DeviceRepository deviceRepo;
+	@Autowired
+	private UserPlanService userPlanService;
 
 	public Device create(Device device) {
-		return deviceRepo.save(device);
+		int upId = device.getUserPlan().getId();
+		UserPlan up = userPlanService.findById(upId);
+		if(up.getDevices().size() < up.getPlan().getMax_devices()) {
+			
+			return deviceRepo.save(device);
+		}
+		
+		return new Device();
 	}
 
 	// might be custom to find all devices associated with the user
-	
+
 	public List<Device> findAll() {
 		return deviceRepo.findAll();
 	}
-	
+
 	public Device findById(int id) {
 		Device returnUser;
 		returnUser = deviceRepo.findById(id).isPresent() ? deviceRepo.findById(id).get() : new Device();
 		return returnUser;
 	}
-	
+
 	public HttpStatus delete(Integer id) {
 		if (deviceRepo.findById(id).isPresent()) {
 			deviceRepo.deleteById(id);
@@ -38,7 +47,7 @@ public class DeviceService {
 		}
 		return HttpStatus.BAD_REQUEST;
 	}
-	
+
 	public HttpStatus update(Device device, Integer id) {
 		if (deviceRepo.findById(id).isPresent() && device.getId() == id) {
 			device.setId(id);
